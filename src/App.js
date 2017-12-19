96791ddaf99a022f2f2c8ff07adb6caa6b69138a
Claudio Díaz <cadiazc@bancochile.cl>
2017-12-19 15:09:42 -0300

    Integración con firebase para agregar imagenes en tiempo real

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            user: null
+            user: null,
+            pictures: []
         };
 
         this.handleAuth = this.handleAuth.bind(this);
         this.handleLogout = this.handleLogout.bind(this);
+        this.handleUpload = this.handleUpload.bind(this);
     }
 
     componentWillMount() {
         firebase.auth().onAuthStateChanged(user => {
             this.setState({user});
+        });
+
+        firebase.database().ref('pictures').on('child_added', snapshot => {
+            this.setState({
+                pictures: this.state.pictures.concat(snapshot.val())
+            });
         });
     }
 
@@ -35,6 +43,33 @@ class App extends Component {
             .catch(error => console.log(`Error ${error.code}: ${error.message}`));
     }
 
+    handleUpload(event) {
+        const file = event.target.files[0];
+        const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+        const task = storageRef.put(file);
+
+        task.on('state_changed', snapshot => {
+                let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                this.setState({uploadValue: percentage})
+            }, error => {
+                console.log(error.message);
+            }, () => {
+                const record = {
+                    photoURL: this.state.user.photoURL,
+                    displayName: this.state.user.displayName,
+                    image: task.snapshot.downloadURL
+                };
+
+                const dbRef = firebase.database().ref('pictures');
+                const newPicture = dbRef.push();
+                console.log(record);
+                console.log(newPicture);
+
+                newPicture.set(record);
+            }
+        );
+    }
+
     renderLoginButton() {
 
         if (this.state.user) {
@@ -44,7 +79,19 @@ class App extends Component {
                     <img src={this.state.user.photoURL} alt={this.state.user.displayName}/>
                     <p>{this.state.user.displayName}</p>
                     <button onClick={this.handleLogout}>Salir</button>
-                    <FileUpload/>
+                    <FileUpload onUpload={ this.handleUpload }/>
+                    {
+                        this.state.pictures.map(picture => (
+                            <div key={picture.image}>
+                                <img src={picture.image}/>
+                                <br/>
+                                <img src={picture.photoURL} alt={picture.displayName}/>
+                                <br/>
+                                <span>{picture.displayName}</span>
+                            </div>
+
+                        )).reverse()
+                    }
                 </div>
 
             );
